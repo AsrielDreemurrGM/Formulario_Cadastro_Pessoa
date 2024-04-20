53d564d6398b41106e64afa6ac8227d11ede7054
AsrielDreemurrGM <eduardo.motta.980315@gmail.com>
2024-04-20 04:08:22 -0300

    Finalizado JQuery Para Mascarar e Validar Formulário

--- js/main.js
+++ js/main.js
@@ -2,4 +2,41 @@ $(document).ready(function() {
     $('.menuHamburger').click(function() { // Detectar quando o menu é clicado
         $('.formCadastro').slideToggle();
     }) 
+
+    $('#telefone').mask('(00) 00000-0000');
+    $('#cpf').mask('000.000.000-00');
+    $('#cep').mask('00000-000');
+    $('#telefone').mask('(00) 00000-0000');
+
+    $('.formCadastro').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
+            endereco: {
+                required: true,
+            },
+            cep: {
+                required: true,
+            },
+        },
+        messages: {
+            nome: 'Por favor insira o seu nome',
+            telefone: 'Por favor digite seu telefone',
+            email: 'Por favor insira um email',
+            cpf: 'Por favor digite seu CPF',
+            endereco: 'Por favor insira seu endereço',
+            cep: 'Por favor digite seu CEP',
+        }
+    })
 })
